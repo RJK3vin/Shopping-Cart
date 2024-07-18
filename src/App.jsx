da6e664330bfb37cec16c6345a592bc086891fad
Kevin Ritthaworn <rj.ritthaworn@icloud.com>
2024-07-18 19:39:53 -0400

    used hard coded data for products

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,50 @@
 import './App.css';
 import { useState } from "react";
 import Navbar from "./Navbar";
-import get_products_from_api from './product-api';
+import products_api from './product-api';
+import { useEffect } from 'react';
 
-function ShowProducts({show, setShow, products, handleClick }) {
+function ShowProducts({show, setShow, datas, manageClick }) {
   return show ? <div> <h3 style = {{ color: "red" }}>The Best Clothing Shopping Page in the World</h3>  <button onClick={setShow}>Shop Now</button> </div> 
   : 
-  <div> 
-    {products.map((product) => (
-      <button key={product.id} products={products} onClick = {(event) => handleClick(event, product)}>{product.name} ${product.price}</button>
+  <>
+  <div>
+  {datas.map((data) => (
+      <button style = {{ width: 150, height: 100 }}key={data.id} onClick = {(event) => manageClick(event, data)}>{data.title} <img style = {{ width: 100, height: 50 }}src = {data.image}/> ${data.price}</button>
     ))}
   </div>
-}  
+  </>
+}
 
 export default function App() {
-
   const [show, setShow] = useState(true)
   const [counter, setCounter] = useState(0)
   const [total, setTotal] = useState(0)
-  const [products, setProducts] = useState([
-            {"name": "Add Shirt to cart", "id": 1, "price" : 8},
-            {"name": "Add shorts to cart", "id": 2, "price" : 10},
-            {"name": "Add Underwear to cart", "id": 3, "price" : 5},
-        ])
+
+  const [datas, setDatas] = useState([])
+  useEffect(() => {
+    setDatas(products_api);
+  }, []);
+
   const [cart, setCart] = useState({})
 
-  function testing(event, product) {
-      if (product["name"] in cart) {
-        cart[product["name"]].count++
-        cart[product["name"]].totalPrice += product["price"]
-        setCart({...cart})
-      } else {
-        cart[product["name"]]= {
-          count: 1,
-          totalPrice: product["price"],
-          price: product["price"]
-        }         
-      }
-      setTotal(total + product["price"])
-      setCounter(counter + 1)
+  function secondtest(event, data) {
+    if ([data["title"]] in cart) {
+      cart[data["title"]].count++
+      cart[data["title"]].totalPrice += data["price"]
       setCart({...cart})
+    } else {
+      cart[data["title"]] = {
+        count: 1,
+        totalPrice: data["price"],
+        price: data["price"]
+      }
     }
-  
+    setTotal(total + data["price"])
+    setCounter(counter + 1)
+    setCart({...cart})
+  }
+
   function Upcount(event, cart, key) {
     cart[key].count++
     cart[key].totalPrice += cart[key].price
@@ -53,6 +56,7 @@ export default function App() {
     if (cart[key].count === 1) {
       setTotal(total - cart[key].price)
       delete cart[key]
+      setCounter(counter - 1)  
     } else {
       cart[key].count--
       cart[key].totalPrice -= cart[key].price
@@ -69,7 +73,7 @@ export default function App() {
       height: "100vh"
     }}>
     <Navbar setShow = {setShow} counter = {counter} />   
-    <ShowProducts show = {show} setShow={() => setShow(false)} products={products} handleClick={testing}/>
+    <ShowProducts show = {show} setShow={() => setShow(false)} datas={datas} manageClick={secondtest}/>
     <p style = {{ color: "red" }}>Cart: </p>
     {Object.keys(cart).map((key) => (
           <p key={key} style = {{ color: "red" }}>{key}: ${cart[key].totalPrice}<button style = {{ width: 20, height: 20 }} onClick={(event) => {Downcount(event, cart, key)}}>-</button> {cart[key].count} <button style = {{ width: 20, height: 20}} onClick={(event) => {Upcount(event, cart, key)}}>+</button></p> 
